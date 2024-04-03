4bb8477ff01b177044394a953ac8a581f86efe09
Smith <53247447+GuiSmith@users.noreply.github.com>
2024-04-03 08:51:48 -0300

    Add files via upload

---
+++ smtp/index.js
@@ -0,0 +1,13 @@
+import {setNavbar ,getAllSmtps, createTable} from '../functions.js';
+
+main();
+async function main(){
+    const list = await getAllSmtps('../');
+    const headers = ['ID','SMTP','E-mail de envio'];
+    const table = createTable(list, headers);
+    table.classList.add('grid-table');
+    document.querySelector('.table-container').appendChild(table);
+    const tableRows = document.querySelectorAll('table tr:not(:first-child)');
+}
+
+setNavbar(false);
