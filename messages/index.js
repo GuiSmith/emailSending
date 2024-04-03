f35c6a7a0a89c2471e7f2de352d3e5ba274e91e4
Smith <53247447+GuiSmith@users.noreply.github.com>
2024-04-03 08:52:14 -0300

    Add files via upload

---
+++ messages/index.js
@@ -0,0 +1,15 @@
+import {setNavbar ,getAllMessages, createTable} from '../functions.js';
+
+main();
+async function main(){
+    /*
+    const list = await getAllMessages();
+    const headers = ['ID','SMTP','Assunto', 'Texto'];
+    const table = createTable(list, headers);
+    table.classList.add('grid-table');
+    document.querySelector('.table-container').appendChild(table);
+    const tableRows = document.querySelectorAll('table tr:not(:first-child)');
+    */
+}
+
+setNavbar(false);
